37b44633f6f968027eda31b6d1ccfc38e4fd4da6
Jordan Gania <102545401+jgania@users.noreply.github.com>
2023-03-21 08:38:42 -0600

    Updated schema

--- create_tables.sql
+++ create_tables.sql
@@ -1,27 +1,34 @@
 CREATE TABLE video_game_sales (
-    name VARCHAR(100) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     na_sales NUMERIC(10, 2) NOT NULL,
     eu_sales NUMERIC(10, 2) NOT NULL,
     jp_sales NUMERIC(10, 2) NOT NULL,
     other_sales NUMERIC(10, 2) NOT NULL,
     global_sales NUMERIC(10, 2) NOT NULL
 );
-
+-- drop table video_game_reviews
 CREATE TABLE video_game_reviews (
-    name VARCHAR(100) PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     critic_score NUMERIC(3, 1),
-    critic_count INTEGER,
+    critic_count NUMERIC,
     user_score NUMERIC(3, 1),
-    user_count INTEGER
+    user_count NUMERIC
 );
 
 CREATE TABLE video_game_details (
-    name VARCHAR(100) PRIMARY KEY,
-    year INTEGER,
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(1000) NOT NULL,
     platform VARCHAR(50),
     year_of_release INTEGER,
-    genre VARCHAR(50),
-    publisher VARCHAR(50),
-    developer VARCHAR(50),
+    genre VARCHAR(500),
+    publisher VARCHAR(500),
+    developer VARCHAR(500),
     rating VARCHAR(10)
 );
+
+select * from video_game_details
+select * from video_game_sales
+select * from video_game_reviews
+
